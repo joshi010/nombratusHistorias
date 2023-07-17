bf8da9c9b87c633615feb6ca8c76d675615f1dfa
joshi010 <101081177+joshi010@users.noreply.github.com>
2023-07-17 09:35:43 -0600

    Add files via upload

--- DíaDi/app.js
+++ DíaDi/app.js
@@ -6,14 +6,17 @@ let time = 86400000;
 
 check.onchange = () => {
     let d = new Date(`${check.value}T00:00:00`);
+    console.log(d);
     let calc = ((d.getTime() - date.getTime())/time).toFixed(0);
     display.innerHTML = (calc*1)+1;
 }
 
 const makeMath = (date) => {
-    let calc = Date.now() - date.getTime();
-    console.log(calc);
-    return (calc/time).toFixed(0);
+    let d = new Date();
+    d.setHours(0,0,0);
+    let calc = d.getTime() - date.getTime();
+    dia.innerHTML = ((calc/time).toFixed(0)*1)+1;
 }
 
-dia.innerHTML = makeMath(date);
+makeMath(date);
+setInterval(makeMath, 5000, date);
